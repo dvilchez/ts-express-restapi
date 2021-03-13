11295b68eefb6fc4325a528bd990ffde56e89f8e
David Vílchez <dvilchez@xuaps.com>
2021-03-13 14:17:52 +0100

    Add infrastructure

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import cors from "cors";
 import { appRouter } from './src/infrastructure/routes'
+import { restore } from 'sinon';
 
 const port = process.env.PORT || 8080;
 
@@ -10,6 +11,11 @@ app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors());
 app.use("/", appRouter);
+app.use((error: Error, req: Request, res: Response, next: NextFunction): Response => {
+  return res.status(500).json({
+      message: 'Something went wrong',
+  });
+})
 
 app.listen(port, () => {
     console.log("server started at http://localhost: %s", port);
--- src/infrastructure/routes.ts
+++ src/infrastructure/routes.ts
@@ -1,14 +1,44 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import { MakeANewRegistration } from '../application'
+import { RegistrationAlreadyExists } from "../domain/exceptions";
+import { Courses, Teachers } from './inMemoryRepos'
 
 const router: express.Router = express.Router();
+const courses = new Courses()
+const teachers = new Teachers()
 
-router.put("/registration", (req: Request, res: Response) => {
+const wrapRoute = (fn: (req: Request, res: Response, next: NextFunction) => void) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await fn(req, res, next)
+        } catch (e) {
+            next(e)
+        }
+    }
+}
 
-    res.json({
-        status: "Test Message"
-    });
-});
+router.post("/registration",
+    body('teacher').isEmail(),    
+    body('course').isLength({ min: 5}),
+    wrapRoute(async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+
+        const command = new MakeANewRegistration(courses, teachers)
+        try{
+            const result = await command.execute(req.body.teacher, req.body.course)
+            res.json(result);
+        } catch (err) {
+            if (err instanceof RegistrationAlreadyExists){
+                return res.status(401).json({message: err.message})
+            }
+            throw err
+        }
+    }
+));
 
 export const appRouter = router;
